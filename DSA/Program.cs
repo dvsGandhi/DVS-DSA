2cc8356db43b548ebdbd476d46828c972161ec88
dvsGandhi <divyeshvgandhi@gmail.com>
2024-03-17 11:32:33 +0530

    1-sorting

--- DSA/Program.cs
+++ DSA/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using DSA.Class;
 
 namespace DSA
 {
@@ -6,6 +7,10 @@ namespace DSA
     {
         static void Main(string[] args)
         {
+            Sorting s1 = new Sorting();
+            int[] arr = { 10, 5, -25, -7, 19, 2, -10 };
+            s1.AssendingSorting(arr);
+            s1.dispArray(arr);
             Console.WriteLine("Hello World!");
         }
     }
