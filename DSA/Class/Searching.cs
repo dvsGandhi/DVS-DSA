2b84eff4c6bc1dbc52a875d1f9fca403267a5fcc
dvsGandhi <divyeshvgandhi@gmail.com>
2024-03-17 12:06:13 +0530

    2 - sorting

---
+++ DSA/Class/Searching.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSA.Class
+{
+    class Searching
+    {
+        public int FindMaxNumber(int[] arr)
+        {
+            int len = arr.Length;
+            for (int i = 0; i < len - 1; i++)
+            {
+                int maxNumber = i;
+                for (int j = i + 1; j < len; j++)
+                {
+                    if (arr[j] > arr[maxNumber])
+                    {
+                        maxNumber = j;
+                    }
+                }
+                return arr[maxNumber];
+            }
+            return 0;
+        }
+    }
+}
--- DSA/Program.cs
+++ DSA/Program.cs
@@ -8,9 +8,12 @@ namespace DSA
         static void Main(string[] args)
         {
             Sorting s1 = new Sorting();
-            int[] arr = { 10, 5, -25, -7, 19, 2, -10 };
+            Searching ser = new Searching();
+            int[] arr = { 10, 5, -25, -7, 19, -10, -60, 30, 55, -40 };
+            Console.WriteLine("max number = "+ ser.FindMaxNumber(arr));
             s1.AssendingSorting(arr);
             s1.dispArray(arr);
+            
             Console.WriteLine("Hello World!");
         }
     }
