b0de0493001c6006ff02512492224c97fe0e528a
divyesh <divyesh@bbd.co.za>
2024-03-28 19:24:22 +0530

    2d array

---
+++ DSA/Class/twoDarray.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSA.Class
+{
+    internal class twoDarray
+    {
+        public void matrix()
+        {
+            int[,] matrix = new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 0 } };
+            int sum = 0;
+            for (int i = 0;i<matrix.GetLength(0);i++)
+            {
+                int product = matrix[i,0];
+                for(int j = 1; j < matrix.GetLength(1); j++)
+                {
+                    int k = matrix[i,j];
+                    product *= matrix[i,j];
+                }
+                sum += product;
+            }
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
--- DSA/Program.cs
+++ DSA/Program.cs
@@ -7,20 +7,28 @@ namespace DSA
     {
         static void Main(string[] args)
         {
-            Sorting s1 = new Sorting();
-            Searching ser = new Searching();
-            int[] arr = { 10, 5, -25, -7, 19, -10, -60, 30, 55, -40 };
-            Console.WriteLine("max number = "+ ser.FindMaxNumber(arr));
-            Console.WriteLine("Sum of total number = "+ ser.TotalSum(arr));
+            #region searching,sorting
+            //Sorting s1 = new Sorting();
+            //Searching ser = new Searching();
+            //int[] arr = { 10, 5, -25, -7, 19, -10, -60, 30, 55, -40 };
+            //Console.WriteLine("max number = "+ ser.FindMaxNumber(arr));
+            //Console.WriteLine("Sum of total number = "+ ser.TotalSum(arr));
 
-            int[] arr1 = arr;
-            Console.WriteLine("Assending Order");
-            s1.AssendingSorting(arr);
-            s1.dispArray(arr);
-            
-            Console.WriteLine("descending Order");
-            s1.DescendingSorting(arr1);
-            s1.dispArray(arr1);
+            //int[] arr1 = arr;
+            //Console.WriteLine("Assending Order");
+            //s1.AssendingSorting(arr);
+            //s1.dispArray(arr);
+
+            //Console.WriteLine("descending Order");
+            //s1.DescendingSorting(arr1);
+            //s1.dispArray(arr1);
+            #endregion
+
+            #region 2dArray
+            twoDarray twoDarray = new twoDarray();
+            twoDarray.matrix();
+
+            #endregion
         }
     }
 }
